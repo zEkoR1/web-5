740241d56fc2a07e1cd4570d3069271251fd6f13
PC <komarovitstudies@gmail.com>
2025-05-12 22:11:17 +0300

    Implemented raw HTTP GET over TCP

--- go2web.js
+++ go2web.js
@@ -10,3 +10,15 @@ function usage() {
   const args = process.argv.slice(2);
   if (args.length === 0 || args.includes('-h')) usage();
   
+const net = require('net');
+
+async function httpGetRaw(urlStr) {
+  const u = new URL(urlStr);
+  if (u.protocol !== 'http:') throw new Error('only http for now');
+  const socket = net.createConnection(80, u.hostname);
+  const req = `GET ${u.pathname || '/'} HTTP/1.1\r\nHost: ${u.hostname}\r\nConnection: close\r\n\r\n`;
+  socket.write(req);
+  let data = '';
+  for await (const chunk of socket) data += chunk.toString('utf8');
+  return data.split('\r\n\r\n')[1]; 
+}
